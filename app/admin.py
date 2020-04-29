6c0fb412a526b5cf1f46ec5c4935f8862e67a91f
Vladislav <1v1expert@gmail.com>
2020-04-29 15:38:39 +0300

    fixed admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
-from app.models import Steganographic
+from app.models import Steganographic, MainLog
 
 
-class FeedBackAdmin(admin.ModelAdmin):
-    list_display = ['action_time', 'email', 'is_subscriber', 'text']
-
-
-# admin.site.register(FeedBack, FeedBackAdmin)
 admin.site.register(Steganographic, )
+admin.site.register(MainLog, )
