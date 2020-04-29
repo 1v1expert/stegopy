73fa202eff0b4775841c8b171262edb52670cbd7
Vladislav <1v1expert@gmail.com>
2020-04-29 15:46:00 +0300

    upd adminpanel

--- app/admin.py
+++ app/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from app.models import Steganographic, MainLog
 
 
+class MainLogAdmin(admin.ModelAdmin):
+    list_display = ['action_time', 'client_address', 'message', 'raw', 'has_errors']
+
+
 admin.site.register(Steganographic, )
-admin.site.register(MainLog, )
+admin.site.register(MainLog, MainLogAdmin)
